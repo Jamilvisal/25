e1a40e8787b1cd9fd79dcc6751ca05abf4bb5329
Jamilvisal <139256724+Jamilvisal@users.noreply.github.com>
2023-07-15 15:30:47 +0430

    Update 25.py
    git clone https://github.com/Jamilvisal/25.git
    cd 25
    python 25.py

--- 25.py
+++ 25.py
@@ -10,7 +10,7 @@ try:
     import requests
     from concurrent.futures import ThreadPoolExecutor as ThreadPool
     import mechanize
-    from requests.exceptions import ConnectionError
+    from requests..exceptions import ConnectionError
 except ModuleNotFoundError:
     os.system('pip install mechanize requests futures==2 > /dev/null')
     os.system('python run.py')
